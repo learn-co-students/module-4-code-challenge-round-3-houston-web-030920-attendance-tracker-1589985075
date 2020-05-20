643713fc335a7d650669482adf95fb9ed39828d2
GianMoran <gpmoran@uh.edu>
2020-05-20 11:02:14 -0500

    finish

--- src/components/CourseContainer.js
+++ src/components/CourseContainer.js
@@ -4,12 +4,53 @@ import CourseSelector from "./CourseSelector";
 import StudentsList from "./StudentsList";
 
 class CourseContainer extends Component {
+  state={
+    courses:[],
+    copyCourses:[],
+    students:[],
+    copyStudents:[],
+    attending: false
+  }
+
+  componentDidMount(){
+  this.fetchCourses()
+  this.fetchStudents()
+  }
+  fetchCourses = ()=>{
+    fetch("http://localhost:6001/courses")
+    .then(resp=>resp.json())
+    .then(courses=>this.setState({courses, copyCourses:courses}))
+  }
+  fetchStudents = ()=>{
+    fetch("http://localhost:6001/students")
+    .then(resp=>resp.json())
+    .then(students=>this.setState({students, copyStudents: students}))
+  }
+  handleClick = () =>{
+    let bool = !this.state.attending 
+    this.setState({
+      attending: bool
+    })
+  }
+  filterCourses = (e)=>{
+    console.log("Filter courses runs",e.target.value)
+    this.setState({
+      copyCourses: this.state.courses.filter(course=> course.id)
+    })
+    this.filterStudents()
+  }
+filterStudents = ()=>{
+console.log("Shows filter students")
+// this.setState({
+//   copyStudents: this.state.students.filter(student=> student.course)
+// })
+}
   render() {
     return (
       <div className="ui grid container">
         <CourseDetails />
-        <CourseSelector />
-        <StudentsList />
+        <CourseSelector courses={this.state.copyCourses} filterCourses={this.filterCourses}/>
+        <StudentsList students={this.state.copyStudents} handleClick={this.handleClick}/>
       </div>
     );
   }
